33c338c1781b7e20e0af9b75ec79eb8ca88b289e
intosmoke <exxtasiss9@gmail.com>
2026-03-13 17:53:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from discord.ext import commands
 bt = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
 @bt.event
-async def on_ready(): print(f'On {bt.user}')
+async def on_ready():
+    await bt.tree.sync() 
+    print(f'On {bt.user}')
 
-@bt.tree.command(name='404')
+@bt.tree.command(name='403')
 async def spam(i: discord.Interaction):
-    await i.response.send_message("intosmoke", ephemeral=True)
+    await i.response.send_message("on top", ephemeral=True)
     for _ in range(20):
-        await i.followup.send("[/404 Pwned This](https://discord.gg/QXWz2UqxGb)")
+        await i.followup.send("# [Order403 Owns This](https://t.me/ord403)")
         await asyncio.sleep(0.1)
 
 bt.run("") #Your bot token
